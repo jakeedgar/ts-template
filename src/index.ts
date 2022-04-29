fc7648c569d1b13a06dc6aa67bacaf5a78f19d4e
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-29 00:55:44 -0700

    add prettier extension and plugins

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 class Helper {
-
-    public component () : HTMLDivElement {
-        const element = document.createElement('div')
-        element.innerHTML = 'Hello, I am a template.'
-        return element
-    }
+  public component(): HTMLDivElement {
+    const element = document.createElement("div");
+    element.innerHTML = "Hello, I am a template.";
+    return element;
+  }
 }
-document.body.appendChild(new Helper().component())
+document.body.appendChild(new Helper().component());
 
 class New {
-
-    public component () : HTMLElement {
-        const element = document.createElement('H1')
-        element.innerHTML = 'This is how you would do it'
-        return element
-        // In this case it is just an HTML Element that is being created. 
-    }
+  public component(): HTMLElement {
+    const element = document.createElement("H1");
+    element.innerHTML = "This is how you would do it";
+    return element;
+    // In this case it is just an HTML Element that is being created.
+  }
 }
-document.body.appendChild(new New().component())
+document.body.appendChild(new New().component());
